f4a0cabb19eb4dc96e6edfdfab23f2bc317dcb54
ram5353 <ramkiran5353@gmail.com>
2022-03-30 13:18:22 -0400

    folder data model

---
+++ src/main/java/com/example/messageapp/folders/Folder.java
@@ -0,0 +1,30 @@
+package com.example.messageapp.folders;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table(value = "folders_by_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Folder {
+
+    @PrimaryKeyColumn(name = "user_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String userId;
+
+    @PrimaryKeyColumn(name = "label", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
+    private String label;
+
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String color;
+
+
+
+}
